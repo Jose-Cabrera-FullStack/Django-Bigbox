d4d0f2eb423005c14daa05c2ad067c5431c0ab15
unknown <soporte@MININT-DTQ6N9E.lanacion.com.ar>
2020-09-12 18:56:34 -0300

    se arreglo el Storage

--- ejercicioBigBox/settings.py
+++ ejercicioBigBox/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStore'
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
